5e10f456a4adf9b9878c4b8cdf3b5db410e18732
Anay Misra <misraanay@gmail.com>
2026-09-14 15:19:21 -0400

    add temporary repo cloning for scans

--- app/tasks.py
+++ app/tasks.py
@@ -1,27 +1,29 @@
-from app.celery_app import app as celery_app
 from app.models import Scan
 from app.database import SessionLocal
 from uuid import UUID
 from app.celery_app import app
 from app.models import ScanStatus
+import os
+import tempfile
+import subprocess
 
 @app.task()
 def process_scan(scan_id: UUID):
     with SessionLocal() as db:
         #set session status as complete
         scan = db.get(Scan, scan_id)
-
         if scan is None:
             raise ValueError(f"Scan {scan_id} not found")
-
         scan.status = ScanStatus.RUNNING
         db.commit()
-
-        # background logic call delay for 5 seconds
-        import time
-        time.sleep(60)
-
+        url = scan.url
         success = True
+        # background logic git clone into a temporary directory
+        with tempfile.TemporaryDirectory() as tmpdir:
+            try:
+                subprocess.run(["git", "clone", "--depth", "1", url, tmpdir], check=True)
+            except subprocess.CalledProcessError as e:
+                success = False
         if success:
             scan.status = ScanStatus.COMPLETED
         else:
